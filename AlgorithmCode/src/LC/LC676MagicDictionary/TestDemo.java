074cfa285ace281dc8a5ef273a37040a4bb055eb
lsgfHW <lsgf@hw.com>
2022-07-11 11:17:56 +0800

    ListNode

---
+++ AlgorithmCode/src/LC/LC676MagicDictionary/TestDemo.java
@@ -0,0 +1,9 @@
+package LC.LC676MagicDictionary;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        MagicDictionary m = new MagicDictionary();
+        m.buildDict(new String[]{"hello", "world", "hi", "nihao"});
+        System.out.println(m.dic);
+    }
+}

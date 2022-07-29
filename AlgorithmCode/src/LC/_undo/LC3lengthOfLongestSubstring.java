9b289736f29d99fc2cd074c19cf0ba74398a3370
lsgfHW <lsgf@hw.com>
2022-07-29 15:29:59 +0800

    add new lcs

--- AlgorithmCode/src/LC/_undo/LC3lengthOfLongestSubstring.java
+++ AlgorithmCode/src/LC/_undo/LC3lengthOfLongestSubstring.java
@@ -32,10 +32,55 @@ s 由英文字母、数字、符号和空格组成
 通过次数1,860,956提交次数4,784,678
  */
 public class LC3lengthOfLongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
+    public static void main(String[] args) {
+        LC3lengthOfLongestSubstring l = new LC3lengthOfLongestSubstring();
+        String str = "abcdefgcab";
+        System.out.println((int) 'a');
+        System.out.println((int) 'z');
+        l.lengthOfLongestSubstringLCUser(str);
+    }
 
+
+    //cabsjkaljs
+    public int lengthOfLongestSubstring(String s) {
+        int start = 0;
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                for (int k = i + 1; k < j; k++) {
+                    if (s.charAt(k) == s.charAt(j)) {
+                        if (k - i > longest) {
+                            longest = k - i;
+                        }
+                        break;
+                    }
+                }
+
+            }
+        }
 
 
         return 0;
+    }
+
+
+    public int lengthOfLongestSubstringLCUser(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+
+        return res;
     }
 }

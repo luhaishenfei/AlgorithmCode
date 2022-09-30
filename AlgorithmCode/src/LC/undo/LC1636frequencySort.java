e80a6a0ff21289e0f660e84ac81a270539d09b87
朱浩闻 <zhuhaowen@hotwind.net>
2022-09-30 11:08:08 +0800

    finished lc1636

--- AlgorithmCode/src/LC/undo/LC1636frequencySort.java
+++ AlgorithmCode/src/LC/undo/LC1636frequencySort.java
@@ -5,14 +5,34 @@ import java.util.*;
 public class LC1636frequencySort {
     public static void main(String[] args) {
         LC1636frequencySort l = new LC1636frequencySort();
-       // l.frequencySort(new int[]{1,  2, 2, 1,3, 3});
+        l.frequencySort(new int[]{1, 2, 2, 3, 3});
 
+        //test PriorityQueue
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        priorityQueue.offer(1);
+        priorityQueue.offer(3);
+        priorityQueue.offer(2);
+        priorityQueue.offer(5);
+        priorityQueue.offer(7);
+        priorityQueue.offer(4);
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll());
+        }
     }
 
 
     //nums = [2,3,1,3,2]
     //[1,3,3,2,2]
 
+    /*
+    	执行耗时:5 ms,击败了86.01% 的Java用户
+	内存消耗:41.5 MB,击败了77.24% 的Java用户
+     */
     public int[] frequencySort(int[] nums) {
 
         Map<Integer, Integer> map = new HashMap();
@@ -24,7 +44,7 @@ public class LC1636frequencySort {
                 map.replace(i, map.get(i) + 1);
             }
         }
-        PriorityQueue<Integer[]> priorityQueue = new PriorityQueue(new Comparator<Integer[]>() {
+        PriorityQueue<Integer[]> priorityQueue = new PriorityQueue<Integer[]>(new Comparator<Integer[]>() {
             @Override
             public int compare(Integer[] o1, Integer[] o2) {
                 if (o1[1] > o2[1]) {
@@ -32,7 +52,7 @@ public class LC1636frequencySort {
                 } else if (o1[1] < o2[1]) {
                     return -1;
                 } else {
-                    return o2[0]- o1[0] ;
+                    return o2[0] - o1[0];
                 }
             }
         });
@@ -42,8 +62,8 @@ public class LC1636frequencySort {
             priorityQueue.add(new Integer[]{i, map.get(i)});
         }
         int idx = 0;
-        for (Integer[] ints :
-                priorityQueue) {
+        while (!priorityQueue.isEmpty()) {
+            Integer[] ints = priorityQueue.poll();
             for (int i = 0; i < ints[1]; i++) {
                 nums[idx] = ints[0];
                 idx++;

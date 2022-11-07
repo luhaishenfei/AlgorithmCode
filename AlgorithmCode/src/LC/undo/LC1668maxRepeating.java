5af16a6af19f1eaa3f9dc544326e179b507d78de
朱浩闻 <zhuhaowen@hotwind.net>
2022-11-07 17:06:40 +0800

    new deep into computer principal

--- AlgorithmCode/src/LC/undo/LC1668maxRepeating.java
+++ AlgorithmCode/src/LC/undo/LC1668maxRepeating.java
@@ -6,28 +6,59 @@ public class LC1668maxRepeating {
 
     public static void main(String[] args) {
         LC1668maxRepeating l = new LC1668maxRepeating();
-        l.maxRepeating("abcbcd","bc");
+        System.out.println(l.maxRepeatingLC1("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"));
+        //   "0aaaba 5aaaba 10aaba 14aaaba 19aaaba 24aaaba29 aaaba"
     }
+
+
+
+    public int maxRepeatingLC1(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+
+        int[] f = new int[n];
+        for (int i = m - 1; i < n; ++i) {
+            boolean valid = true;
+            for (int j = 0; j < m; ++j) {
+                if (sequence.charAt(i - m + j + 1) != word.charAt(j)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
+            }
+        }
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+
+
+
+
+
 
     public int maxRepeating(String sequence, String word) {
         char[] sequenceChars = sequence.toCharArray();
-        char[] wordChars=word.toCharArray();
-        int maxRepet=0;
-        for (int i = 0; i <sequence.length() ; i++) {
-            char c =sequenceChars[i];
-            if (c==wordChars[0]){
-                int j = 1;
-                for (; j <word.length() ; j++) {
-                    if (sequenceChars[i+j]!=wordChars[j]){
-                        break;
-                    }
-                }
-                if(j==word.length()) maxRepet++;
-                i=i+j-1;
+        char[] wordChars = word.toCharArray();
+        int maxRepet = 0;
+        int currRepet = 0;
+        for (int i = 0; i < sequence.length(); i++) {
+            int j = 0;
+            while (i < sequenceChars.length && sequenceChars[i] == wordChars[j % word.length()]) {
+                i++;
+                j++;
+                currRepet++;
             }
-        }
-        return maxRepet;
-    }
+            if (j != 0) {
+                maxRepet = currRepet > maxRepet ? currRepet : maxRepet;
+                i = i - j % word.length()-1;
+            }
 
+        }
+        return maxRepet / word.length();
+    }
 
 }
